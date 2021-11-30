9f0946e75dda93556115cd7758b8291ce40fbd3f
Ruben Ramirez <rubenrv95@hotmail.com>
2021-11-30 10:26:01 -0300

    Modelos Modulo y Plan

---
+++ database/migrations/2021_11_30_122524_create_modulos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modulos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Nombre');
+            $table->int('cantidad_de_creditos');
+            $table->int('horas_semanales');
+            $table->string('prerequisitos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modulos');
+    }
+}
